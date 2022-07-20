cdac1f6daaacf87f7f82fca55dd572188d4bd909
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-07-21 01:31:23 +0200

    ngClass added to status

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ArrivalOrDeparture } from '../interface';
 
+type Status = 'Boarding' | 'on position' | 'closed' | 'baggage delivery' | 'departed' | 'landed' | 'ready for Boarding';
+type Color = 'green' | 'red' | 'yellow' | 'orange';
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -13,5 +16,28 @@ export class TableComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {  }
+  ngOnInit() {  }
+
+  statusToClass(status: Status): Color {
+    if(status === 'Boarding'){
+      return 'green';
+    } else if (status === 'on position'){
+      return 'yellow';
+    } else if (status === 'closed'){
+      return 'red';
+    } else if (status === 'baggage delivery'){
+      return 'orange';
+    } else if (status === 'departed'){
+      return 'red';
+    } else if (status === 'landed'){
+      return 'green';
+    } else {
+      return "yellow"
+    }
+  }
+
+  tst(row: any): string {
+    console.log(row);
+    return ''
+  }
 }
